16e3f5c6089fdb195d7fa9218f95a6e371980777
FWNT <yy35808319@gmail.com>
2020-11-16 16:17:01 +0000

    1117

---
+++ L06/sort2.py
@@ -0,0 +1,14 @@
+cell1_1='x'
+cell1_2='x'
+cell1_3='o'
+cell1_4=' '
+cell1_5='o'
+cell1_6=' '
+cell1_7='x'
+cell1_8=' '
+cell1_9=' '
+print(cell1_1,'|',cell1_2,'|',cell1_3)
+print('--+---+--')
+print(cell1_4,'|',cell1_5,'|',cell1_6)
+print('--+---+--')
+print(cell1_7,'|',cell1_8,'|',cell1_9)
