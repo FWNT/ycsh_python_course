ac50b4952909ab7ff66b6dda013024e8a3fc4735
FWNT <yy35808319@gmail.com>
2020-11-23 20:02:42 +0000

    0

--- L07/sort3.py
+++ L07/sort3.py
@@ -1,2 +1,11 @@
 # 撰寫一排序程式，檔案名稱 sort3.py
 # 提示使用者輸入三個數字，將數字由大至小分行顯示
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('c='))
+x=max(a,b,c)
+y=min(a,b,c)
+z=a+b+c-x-y
+print(x)
+print(z)
+print(y)
--- L07/sort4.py
+++ L07/sort4.py
@@ -1,2 +1,24 @@
 # 撰寫一排序程式，檔案名稱 sort4.py
 # 提示使用者輸入四個數字，將數字由大至小分行顯示
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('c='))
+d=float(input('d='))
+x=max(a,b,c,)
+y=min(a,b,c,)
+z=a+b+c-x-y
+if d>x:
+  print(d)
+  print(x)
+  print(z)
+  print(y)
+elif d<y:
+  print(x)
+  print(z)
+  print(y)
+  print(d)
+if d>y && d<z:
+  print(x)
+  print(z)
+  print(d)
+  print(y)
