c8cc984fec7c79b6b58a177de74454e13d6dc150
FWNT <yy35808319@gmail.com>
2020-11-23 19:36:09 +0000

    太神啦

--- L07/is_prime.py
+++ L07/is_prime.py
@@ -2,7 +2,12 @@
 # 提示使用者輸入一數字（100 以內的正整數），判斷此數字是否為質數，
 # 若為質數，顯示 '是質數' ，反之顯示 '不是質數' 。
 x=int(input('請輸入一數字'))
-if x%2 == 0:
-  print('是偶數')
-else:
-  print('是奇數')
+y=int(x*(1/2))
+b=1
+for i in range(y):
+  b=b+1
+  if x%b==0:
+    print('不是質數')
+    break 
+  elif b==y:
+    print('是質數')
